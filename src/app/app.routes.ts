90bcbe5e507b1b06fcea11812cf712e706059302
nathanfqt <natfqt@gmail.com>
2025-03-31 10:43:02 +0200

    Display principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClassementComponent } from './components/classement/classement.component';
+import { GenerateurCourseComponent } from './components/generateur-course/generateur-course.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'classement', component: ClassementComponent },
+  { path: 'generateur-course', component: GenerateurCourseComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

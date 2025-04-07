9e260b4ff8f20c291f3923cdbbf10088babc8cc0
Nathan HUCLEUX <nathan683.hucleux@gmail.com>
2025-04-07 13:17:41 +0200

    récupération projet

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  checkPing(): Promise<boolean> {
+    return new Promise(resolve => {
+      this.httpClient.get("/api/ping", {responseType: "text"})
+        .subscribe({
+          next: (responses: any) => {
+            resolve(responses);
+          },
+          error: () => {
+            resolve(false);
+          }
+        });
+    });
+  }
+}
